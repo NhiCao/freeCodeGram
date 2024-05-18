8e3428fc67c28baecd08b72e1e77659e9e8c30e3
Cao Hai Nhi <nhicao1995@gmail.com>
2024-05-18 22:53:34 +0700

    add Eloquent relationships for User and Portfolio

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function portfolio() {
+        return $this->hasOne(Portfolio::class);
+    }
 }
