762193f78a3056c7bd7f166e3fafad89a6fddada
Cao Hai Nhi <nhicao1995@gmail.com>
2024-05-25 17:04:33 +0700

    authorize actions with a model policy

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -16,15 +16,18 @@ class PortfolioController extends Controller
 
     public function update($username, Request $request)
     {
+        $user = User::where('username', $username)->first();
+        $portfolio = Portfolio::where('user_id', $user->id)->first();
+        if ($request->user()->cannot('update', $portfolio)) {
+            abort(403);
+        }
+
         $validatedData = $request->validate([
             'title' => 'nullable',
             'description' => 'nullable',
             'url' => 'url'
         ]);        
         
-        $user = User::where('username', $username)->first();
-        $portfolio = Portfolio::where('user_id', $user->id)->first();
-
         $portfolio->title = $validatedData['title'];
         $portfolio->description = $validatedData['description'];
         $portfolio->url = $validatedData['url'];
